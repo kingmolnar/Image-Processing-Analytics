d1089216272af953e19a9b4daa59649bb7ed56b3
Peter Molnar <peter@kingmolnar.com>
2020-09-08 21:02:15 -0400

    script crashes on macOS

---
+++ OpenCV/scripts/flann_based_matcher.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+import cv2
+import numpy as np
+
+img1 = cv2.imread('../images/box.png',0)          # queryImage
+img2 = cv2.imread('../images/box_in_scene.png',0) # trainImage
+
+# Initiate SIFT detector
+sift = cv2.SIFT()
+
+# find the keypoints and descriptors with SIFT
+kp1, des1 = sift.detectAndCompute(img1,None)
+kp2, des2 = sift.detectAndCompute(img2,None)
+
+# FLANN parameters
+FLANN_INDEX_KDTREE = 1
+index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
+search_params = dict(checks=50)   # or pass empty dictionary
+
+flann = cv2.FlannBasedMatcher(index_params,search_params)
+
+matches = flann.knnMatch(des1,des2,k=2)
+
+# Need to draw only good matches, so create a mask
+matchesMask = [[0,0] for i in xrange(len(matches))]
+
+# ratio test as per Lowe's paper
+for i,(m,n) in enumerate(matches):
+    if m.distance < 0.7*n.distance:
+        matchesMask[i]=[1,0]
+
+draw_params = dict(matchColor = (0,255,0),
+                   singlePointColor = (255,0,0),
+                   matchesMask = matchesMask,
+                   flags = 0)
+
+img3 = cv2.drawMatchesKnn(img1,kp1,img2,kp2,matches,None,**draw_params)
+
+cv2.imwrite('flann_match.png', img3)
